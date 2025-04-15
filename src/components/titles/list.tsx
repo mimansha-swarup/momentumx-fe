c90550ba1670c91e6c33c5c3e30d99e0156e3f1a
mimansha-swarup <mimanshaswarup@gmail.com>
2025-04-15 20:00:18 +0530

    refactor: remove husky pre-commit script and update lint-staged configuration

--- src/components/titles/list.tsx
+++ src/components/titles/list.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import GeneratedContent from "../dashboard/generatedContent";
 import { useAppSelector } from "@/hooks/useRedux";
 import { getTitlesData } from "@/utils/feature/titles/titles.slice";
